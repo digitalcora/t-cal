d3e1e6988912abdeb124695f965d30fb520daa15
Cora Grant <394835+x@users.noreply.github.com>
2020-01-18 16:37:11 -0500

    Add compat-mode switch

--- src/t_cal/calendar.cr
+++ src/t_cal/calendar.cr
@@ -5,7 +5,7 @@ class TCal::Calendar
 
   private VERSION = 1 # Increment to invalidate existing UIDs
 
-  def initialize(alerts)
+  def initialize(alerts, @compat_mode : Bool)
     @alerts = alerts.select do |alert|
       alert.effect == "SHUTTLE" && alert.definite_active_periods.any?
     end
@@ -16,7 +16,11 @@ class TCal::Calendar
     io.puts "VERSION:2.0"
     io.puts "PRODID:-//TCal//NONSGML MBTA Shuttles Calendar//EN"
     io.puts "X-WR-CALNAME:MBTA Shuttles"
+    @compat_mode ? output_compat_events(io) : output_events(io)
+    io.puts "END:VCALENDAR"
+  end
 
+  private def output_events(io)
     @alerts.each do |alert|
       io.puts "BEGIN:VEVENT"
       io.puts "UID:tcal-v#{VERSION}-#{alert.id}"
@@ -26,9 +30,9 @@ class TCal::Calendar
       io.puts "URL:#{alert.url}" if !alert.url.nil?
       io.puts "DTSTAMP:#{format_time(alert.updated_at)}"
 
-      periods = condense(alert.definite_active_periods)
-      io.puts "DTSTART:#{format_time(periods.first.start)}"
-      io.puts "DTEND:#{format_time(periods.first.end)}"
+      periods = condense_periods(alert.definite_active_periods)
+      io.puts format_prop("DTSTART", periods.first.start)
+      io.puts format_prop("DTEND", periods.first.end)
 
       if periods.size > 1
         recurrences = periods.skip(1).map do |period|
@@ -39,21 +43,68 @@ class TCal::Calendar
 
       io.puts "END:VEVENT"
     end
-
-    io.puts "END:VCALENDAR"
   end
 
+  private def output_compat_events(io)
+    @alerts.each do |alert|
+      compat_periods(alert.definite_active_periods).each do |period|
+        io.puts "BEGIN:VEVENT"
+        io.puts "UID:tcal-v#{VERSION}-#{alert.id}-#{to_unix(period.start)}"
+        io.puts "SEQUENCE:#{alert.updated_at.to_unix}"
+        io.puts "SUMMARY:#{alert.service_effect}"
+        io.puts "DESCRIPTION:#{alert.header}"
+        io.puts "URL:#{alert.url}" if !alert.url.nil?
+        io.puts "DTSTAMP:#{format_time(alert.updated_at)}"
+        io.puts format_prop("DTSTART", period.start)
+        io.puts format_prop("DTEND", period.end) if period.start != period.end
+        io.puts "END:VEVENT"
+      end
+    end
+  end
+
+  private alias Date = Tuple(Int32, Int32, Int32)
+  private record DatePeriod, start : Date, end : Date
   private alias TimePeriod = TCal::JSONAPI::DefinitePeriod
 
-  private def condense(periods)
-    periods.map do |period|
-      TimePeriod.new(adjust_start(period.start), adjust_end(period.end))
-    end.reduce([] of TimePeriod) do |periods, period|
+  private def condense_periods(periods)
+    time_adjust(periods).reduce([] of TimePeriod) do |periods, period|
       if !periods.empty? && period.start == periods[-1].end
         periods << TimePeriod.new(periods.pop.start, period.end)
       else
         periods << period
       end
+    end
+  end
+
+  private def compat_periods(periods)
+    time_adjust(periods).flat_map do |period|
+      split_days(period)
+    end.map do |period|
+      if all_day?(period)
+        DatePeriod.new(period.start.date, period.end.date)
+      else
+        period
+      end
+    end.reduce([] of DatePeriod | TimePeriod) do |periods, period|
+      if !periods.empty? && period.start == periods[-1].end
+        last_period = periods.pop
+
+        if period.is_a?(DatePeriod) && last_period.is_a?(DatePeriod)
+          periods << DatePeriod.new(last_period.start, period.end)
+        elsif period.is_a?(TimePeriod) && last_period.is_a?(TimePeriod)
+          periods << TimePeriod.new(last_period.start, period.end)
+        else
+          raise "unreachable"
+        end
+      else
+        periods << period
+      end
+    end
+  end
+
+  private def time_adjust(periods)
+    periods.map do |period|
+      TimePeriod.new(adjust_start(period.start), adjust_end(period.end))
     end
   end
 
@@ -72,7 +123,43 @@ class TCal::Calendar
     end
   end
 
+  private def all_day?(period)
+    period.start != period.end &&
+      period.start == period.start.at_beginning_of_day &&
+      period.end == period.end.at_beginning_of_day
+  end
+
+  private def split_days(period)
+    start_next_day = period.start.shift(days: 1).at_beginning_of_day
+
+    if period.start.day != period.end.day && period.end != start_next_day
+      [TimePeriod.new(period.start, start_next_day)] +
+        split_days(TimePeriod.new(start_next_day, period.end))
+    else
+      [period]
+    end
+  end
+
+  private def to_unix(time)
+    case time
+    when Date then Time.utc(time[0], time[1], time[2], 0, 0, 0).to_unix
+    when Time then time.to_unix
+    end
+  end
+
+  private def format_prop(name, value)
+    case value
+    when Date then "#{name};VALUE=DATE:#{format_time(value)}"
+    when Time then "#{name}:#{format_time(value)}"
+    end
+  end
+
   private def format_time(time)
-    time.to_utc.to_s("%Y%m%dT%H%M%SZ")
+    case time
+    when Date
+      time[0].to_s + time[1].to_s.rjust(2, '0') + time[2].to_s.rjust(2, '0')
+    when Time
+      time.to_utc.to_s("%Y%m%dT%H%M%SZ")
+    end
   end
 end
--- src/t_cal/handler.cr
+++ src/t_cal/handler.cr
@@ -7,11 +7,13 @@ class TCal::Handler
   private ALERTS_URL = "https://api-v3.mbta.com/alerts?filter[route_type]=0,1,2"
   private CACHE_TIME = Time::Span.new(hours: 0, minutes: 1, seconds: 0)
 
-  @cache : String?
-  @cached_at : Time
+  # User-Agents for which "compat mode" can safely default to false
+  private COMPLIANT_AGENTS = StaticArray[/Google-Calendar-Importer/]
+
+  private record Cache, content : String, time : Time
 
   def initialize
-    @cached_at = CACHE_TIME.ago
+    @caches = {} of Bool => Cache
   end
 
   def call(context)
@@ -19,24 +21,32 @@ class TCal::Handler
       context.response.content_type =
         ($~[1] == "ics" ? "text/calendar" : "text/plain")
 
-      handle(context)
+      compat_mode =
+        case context.request.query_params["compat"]?
+        when "true"  then true
+        when "false" then false
+        else
+          user_agent = context.request.headers["user-agent"]?
+          COMPLIANT_AGENTS.none? { |agent| user_agent =~ agent }
+        end
+
+      handle(context.response, compat_mode)
     else
       call_next(context)
     end
   end
 
-  private def handle(context)
-    if @cache && @cached_at && Time.utc - @cached_at < CACHE_TIME
-      context.response << @cache
+  private def handle(response, compat_mode)
+    if (cache = @caches[compat_mode]?) && Time.utc - cache.time < CACHE_TIME
+      response << cache.content
     else
       HTTP::Client.get(ALERTS_URL) do |api_response|
         case api_response.status
         when HTTP::Status::OK
           alerts = TCal::JSONAPI::Response.from_json(api_response.body_io).data
-          calendar = TCal::Calendar.new(alerts).to_s
-          @cache = calendar
-          @cached_at = Time.utc
-          context.response << calendar
+          calendar = TCal::Calendar.new(alerts, compat_mode).to_s
+          @caches[compat_mode] = Cache.new(calendar, Time.utc)
+          response << calendar
         else
           raise "Unexpected API response: #{api_response.body_io.gets_to_end}"
         end
