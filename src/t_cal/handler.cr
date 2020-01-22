966096731039e425d7c40e0df231a06e31a9cf8e
Cora Grant <394835+x@users.noreply.github.com>
2020-01-21 21:01:28 -0500

    Remove commuter rail

--- src/t_cal/handler.cr
+++ src/t_cal/handler.cr
@@ -4,7 +4,7 @@ require "./calendar"
 class TCal::Handler
   include HTTP::Handler
 
-  private ALERTS_URL = "https://api-v3.mbta.com/alerts?filter[route_type]=0,1,2"
+  private ALERTS_URL = "https://api-v3.mbta.com/alerts?filter[route_type]=0,1"
   private CACHE_TIME = Time::Span.new(hours: 0, minutes: 1, seconds: 0)
 
   # User-Agents for which "compat mode" can safely default to false
