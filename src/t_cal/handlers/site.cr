d925e8085fe19a831ddf871f7b9d2e6e4584d8bf
Cora Grant <394835+x@users.noreply.github.com>
2022-09-16 12:31:59 -0400

    Refactor website template rendering

--- src/t_cal/handlers/site.cr
+++ src/t_cal/handlers/site.cr
@@ -5,23 +5,42 @@ require "markd"
 class TCal::Handlers::Site
   include HTTP::Handler
 
-  @content : String
+  private TEMPLATE_DIR = "#{__DIR__}/../site/"
 
   # Creates a handler instance.
   # The `canonical_origin` is used to construct the iCal URL shown on the page.
   def initialize(canonical_origin : String)
-    content = {{read_file "#{__DIR__}/../site/content.md"}}
-    options = Markd::Options.new(smart: true)
-    @content = Markd.to_html(content, options).sub("%ORIGIN%", canonical_origin)
+    @homepage = Homepage.new(canonical_origin)
   end
 
   # :nodoc:
   def call(context)
     if context.request.path == "/"
       context.response.content_type = "text/html"
-      ECR.embed("#{__DIR__}/../site/template.ecr", context.response)
+      context.response << @homepage
     else
       call_next(context)
+    end
+  end
+
+  private class Homepage
+    def initialize(canonical_origin : String)
+      @content = Content.new(canonical_origin)
+    end
+
+    ECR.def_to_s(TEMPLATE_DIR + "layout.html.ecr")
+  end
+
+  private class Content
+    def initialize(@canonical_origin : String)
+    end
+
+    def to_s(io : IO)
+      io <<
+        Markd.to_html(
+          ECR.render(TEMPLATE_DIR + "content.md.ecr"),
+          Markd::Options.new(smart: true)
+        )
     end
   end
 end
